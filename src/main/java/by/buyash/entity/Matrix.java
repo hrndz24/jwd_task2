3ebe207be239c87691725178ca226fc2058d9127
Mike <natalli.buyanova.01@mail.ru>
2020-02-15 01:08:54 +0300

    Optimize generating random numbers in threads

--- src/main/java/by/buyash/entity/Matrix.java
+++ src/main/java/by/buyash/entity/Matrix.java
@@ -1,26 +1,31 @@
 package by.buyash.entity;
 
 import by.buyash.constant.IntegerConstant;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public enum Matrix {
 
     INSTANCE;
 
-    private int size = IntegerConstant.MATRIX_SIZE.getValue();
-    private int[][] matrix = new int[size][size];
-    ReentrantLock[][] lockers = new ReentrantLock[size][size];
+    private int size;
+    private int[][] matrix;
+    private ReentrantLock[][] lockers;
 
     {
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
+        size = IntegerConstant.MATRIX_SIZE.getValue();
+        matrix = new int[size][size];
+
+        lockers = new ReentrantLock[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 lockers[i][j] = new ReentrantLock();
             }
         }
     }
 
     public void setNumberAt(int row, int column, int number) {
-        //todo
+        //todo checking
         ReentrantLock lock = lockers[row][column];
         lock.lock();
         matrix[row][column] = number;
@@ -38,14 +43,13 @@ public enum Matrix {
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        for(int[] row: matrix){
+        for (int[] row : matrix) {
             stringBuilder.append("[");
-            for(int number:row){
+            for (int number : row) {
                 stringBuilder.append(number).append(" ");
             }
             stringBuilder.append("]").append("\n");
         }
-        //todo stringBuilder.append("\n");
         return stringBuilder.toString();
     }
 }
--- src/main/java/by/buyash/thread/MatrixThread.java
+++ src/main/java/by/buyash/thread/MatrixThread.java
@@ -7,32 +7,30 @@ import java.util.Random;
 public class MatrixThread extends Thread {
 
     private Matrix matrix = Matrix.INSTANCE;
-    private int row;
-    private int column;
+
+    private int diagonalIndex;
     private int number;
     private int rowAndColumnElementsSum;
 
     public MatrixThread(int diagonalIndex, int number) {
         super(String.valueOf(number));
-        this.row = diagonalIndex;
-        this.column = diagonalIndex;
+        this.diagonalIndex = diagonalIndex;
         this.number = number;
     }
 
     @Override
     public void run() {
         System.out.println("Here we go\n");
-        matrix.setNumberAt(row, column, number);
+        matrix.setNumberAt(diagonalIndex, diagonalIndex, number);
+
         // TODO: 14.02.2020 optimize
         Random random = new Random();
-        int column = this.column, row = this.row;
-        int i = random.nextInt(2);
-        if (i == 0) {
-            column = random.nextInt(matrix.getSize());
+        if (random.nextBoolean()) {
+            matrix.setNumberAt(diagonalIndex, generateRandomNumber(), number);
         } else {
-            row = random.nextInt(matrix.getSize());
+            matrix.setNumberAt(generateRandomNumber(), diagonalIndex, number);
         }
-        matrix.setNumberAt(row, column, number);
+
         calculateRowAndColumnElementsSum();
         System.out.println("From " + getName() + Matrix.INSTANCE.toString());
         System.out.println("Ah shit here we go again " + rowAndColumnElementsSum + " \n");
@@ -42,12 +40,17 @@ public class MatrixThread extends Thread {
         return rowAndColumnElementsSum;
     }
 
+    private int generateRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(matrix.getSize());
+    }
+
     private void calculateRowAndColumnElementsSum() {
         int matrixSize = matrix.getSize();
         for (int i = 0; i < matrixSize; i++) {
-            rowAndColumnElementsSum += matrix.getNumberAt(i, column);
-            rowAndColumnElementsSum += matrix.getNumberAt(row, i);
+            rowAndColumnElementsSum += matrix.getNumberAt(i, diagonalIndex);
+            rowAndColumnElementsSum += matrix.getNumberAt(diagonalIndex, i);
         }
-        rowAndColumnElementsSum -= matrix.getNumberAt(row, row);
+        rowAndColumnElementsSum -= matrix.getNumberAt(diagonalIndex, diagonalIndex);
     }
 }
